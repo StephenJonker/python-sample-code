be56eea59a149d57d70bf336809cf18b1b76aa55
Stephen Jonker <stephen.jonker.ca@gmail.com>
2017-09-14 12:11:54 -0400

    Initial checkin

---
+++ hello-world/hello-world.py
@@ -0,0 +1,17 @@
+#/usr/local/bin/python3 
+#
+
+# Written by: Stephen Jonker
+# Written on: Thu 14 Sept 2017
+# Copyright (c) Stephen Jonker 2017 - www.stephenjonker.com
+#
+# Description:
+# 	- Just your basic "Hello World" program. 
+#	- Ya gotta have one of these!
+#
+
+print("Hello World!) 
+
+# 
+# End of script
+#
